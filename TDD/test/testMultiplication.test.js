9cae9f37c0ab0589cb5e10b1cb2e87c643de487f
so0 <syngshm@gmail.com>
2021-01-18 19:07:37 +0900

    4장

--- TDD/test/testMultiplication.test.js
+++ TDD/test/testMultiplication.test.js
@@ -2,9 +2,6 @@ const Dollar = require('../src/Dollar');
 
 test('test Multiplication', () => {
   const five = new Dollar(5);
-  let product = five.times(2);
-  expect(product.amount).toBe(10);
-
-  product = five.times(3);
-  expect(product.amount).toBe(15);
+  expect(five.times(2)).toEqual(new Dollar(10));
+  expect(five.times(3)).toEqual(new Dollar(15));
 });
